5b3c0cb674ea34344b59d06e0caad96e5c571640
MarFueGit <maricelafuentes100@gmail.com>
2022-10-06 21:10:58 -0500

    agregando svg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-// en index.js
+ // en index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
 
 ReactDOM.render(<App />, document.getElementById('app')); 
+
+
+
+
